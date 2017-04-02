f5a0ec557feed9ebed2cd22a47b8b6d37807c178
John Augustine <Jmaugust@syr.edu>
2017-04-02 16:15:32 -0400

    First Draft

--- Final-Project.py
+++ Final-Project.py
@@ -3,16 +3,16 @@ def meal_selector():
     x = input("Are you making Breakfast, Lunch, Dinner, or Dessert? ").upper()
     if x == "BREAKFAST":
         #Search the web by filtering for only Breakfast recipes
-        count == count + 1
+        count == count + 1 #This is just filler until we have a search program
     elif x == "LUNCH":
         #Search the web by filtering for only Lunch recipes
-        count == count - 1
+        count == count - 1 #This is just filler until we have a search program
     elif x == "DINNER":
         #Search the web by filtering for only Dinner recipes
-        count == count * 1
+        count == count * 1 #This is just filler until we have a search program
     elif x == "DESSERT":
         #Search the web by filtering for only Desser recipes
-        count == count / 1
+        count == count / 1 #This is just filler until we have a search program
 
 
 print("Welcome to Food Generator:")
